becdbefde041fb8450c1e5dfc7bea6615f474a50
Nuvemenvio <renanbevi@gmail.com>
2024-01-12 20:27:41 -0300

    Atualizacao Construtor produtoPerecivel

--- src/ProdutoPerecivel.java
+++ src/ProdutoPerecivel.java
@@ -5,6 +5,7 @@ public class ProdutoPerecivel extends Produto {
 
     public ProdutoPerecivel(String nome, double preco, int quantidade, int dataValidade){
         super(nome, preco, quantidade);
+        this.dataValidade = dataValidade;
 
     }
 
